ba43d7faa4464a80214f0fcf43dfc507729fc58a
Cedric Conday <277679649+CedricConday@users.noreply.github.com>
2026-07-07 12:20:01 +0000

    autobackup 2026-07-07T12:20:01Z (vps1)

--- app.config.js
+++
@@ -1,6 +0,0 @@
-// Extends app.json; forces "web" platform for react-native-web review builds.
-module.exports = ({ config }) => {
-  const platforms = new Set([...(config.platforms || ["ios", "android"]), "web"]);
-  config.platforms = [...platforms];
-  return config;
-};
